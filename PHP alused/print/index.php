4e1503e31ccaa0bb9280fa9973c244c63802b897
kristofer.vasser <kristofer.vasser@khk.ee>
2019-02-13 13:12:21 +0200

    foorituled

--- PHP alused/print/index.php
+++ PHP alused/print/index.php
@@ -4,12 +4,13 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-
+        <div class="main">
 <?php
 echo '<div id="punane"></div>';
 echo '<div id="kollane"></div>';
 echo '<div id="roheline"></div>';
 ?>
+        </div>
     </body>
 </html>
 
