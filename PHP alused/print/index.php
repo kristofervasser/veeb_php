c6ac067fef72ba5dd3238cb39d9cd6a0230aa24a
kristofer.vasser <kristofer.vasser@khk.ee>
2019-02-13 12:39:22 +0200

    print hello world

---
+++ PHP alused/print/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kristofer.vasser
+ * Date: 13.02.2019
+ * Time: 12:14
+ */
+echo "Hello World!<br>";
+echo "Hello World!<br>";
+echo "Hello World!<br>";
+
+echo "<hr>";
+
+print "Hello World!<br>";
+print "Hello World!<br>";
+print "Hello World!<br>";
