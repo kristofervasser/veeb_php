97382349db43fa45748b90abcdfb94c576402138
kristofer.vasser <kristofer.vasser@khk.ee>
2019-02-13 14:34:48 +0200

    condition = if, else

---
+++ PHP alused/condition/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kristofer.vasser
+ * Date: 13.02.2019
+ * Time: 14:15
+ */
+$eesnimi = "Kristofer";
+$perenimi = "Vasser";
+$vanus = 20;
+$kaal = 61;
+$sugu = "mees";
+
+if($sugu == "mees") {
+    echo '<p style="color: blue;">';
+} else {
+    echo '<p style="color: red;">';
+}
+
+echo "Minu eesnimi on ".$eesnimi."<br>";
+echo "Minu perekonnanimi on ".$perenimi."<br>";
+echo "Vanuseks mul ".$vanus." aastat<br>";
+echo "Kaalun ".$kaal." kg<br>";
