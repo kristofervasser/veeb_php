872814c3a7beac9143d22329032aed2359d7b7e1
kristofer.vasser <kristofer.vasser@khk.ee>
2019-02-13 15:31:32 +0200

    condition, refresh

--- PHP alused/condition/index.php
+++ PHP alused/condition/index.php
@@ -1,4 +1,29 @@
 <?php
+for($kord = 1; $kord <= 10; $kord++) {
+
+
+    header('Refresh:2');
+    if($kord == 11) break;
+}
+?>
+<style>
+    div {
+        margin: auto;
+        width: 100px;
+        height: 100px;
+        font-size: 50px;
+        padding-top: 15px;
+       text-align: center;
+    }
+    .paaris{
+        background: red;
+    }
+    .paaritu{
+        background: green;
+    }
+</style>
+<?php
+
 //genereerime juhuslik täisarv vahemikus 0 kuni 100
 $arv =rand(0, 100);
 //arvutame jääk 2-ga jagamisel
@@ -7,6 +32,6 @@ $jaak = $arv % 2;
 if($jaak == 0) {
     echo '<div class="paaris">'.$arv.'</div>';
 }   else {
-    echo '<div class="paaritu">'.$arv.'</div>';
+    echo '<div class="paaritu">' . $arv . '</div>';
 
 }
