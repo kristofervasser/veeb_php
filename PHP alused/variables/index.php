33b6985fd3008ecc1c50b9848febd49be19a64dc
kristofer.vasser <kristofer.vasser@khk.ee>
2019-02-13 14:22:17 +0200

    muutuja sisu vaatamine

---
+++ PHP alused/variables/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kristofer.vasser
+ * Date: 13.02.2019
+ * Time: 14:15
+ */
+$eesnimi = "Kristofer";
+$perenimi = "Vasser";
+$vanus = 20;
+$kaal = 61;
+
+//var_dump($eesnimi); // muutuja sisu vaatamine
